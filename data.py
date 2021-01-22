52e096bb55bec0a53bef99215800fd08c4eba323
timoKroecker <t.kroecker@web.de>
2021-01-22 22:35:20 +0100

    news fix

--- data.py
+++ data.py
@@ -82,9 +82,8 @@ birthdays_categories =  [
                         ]
 
 news_soup_ingredients = [
-                            ["https://www.rt1.de/nachrichten/augsburg/", "h3", "headline", " -rt1.de", 5],
                             ["https://focus.de", "h4", "vr", " -Focus", 30],
-                            ["https://www.spiegel.de/", "span", "hover:opacity-moderate focus:opacity-moderate", " -Spiegel", 30],
+                            ["https://www.spiegel.de/", "span", "hover:opacity-moderate focus:opacity-moderate", " -Spiegel", 5],
                             ["https://n-tv.de", "span", "teaser__headline", " -ntv", 30],
                             ["https://www.welt.de/", "div", "o-headline o-teaser__headline c-dreifaltigkeit__headline c-teaser-default__headline", " -Welt", 30]
                         ]
