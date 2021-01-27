48ccfc450928d5bfe8cf91612321a82c0849aec2
timoKroecker <t.kroecker@web.de>
2021-01-27 21:14:34 +0100

    debug

--- news_scraper.py
+++ news_scraper.py
@@ -34,7 +34,7 @@ def cook_soup(index):
     #check if the html text is just a blank header
     if(len(str(soup)) < 10000):
         print("COOK-SOUP-EXCEPION:\n")
-        print("\t" + ingr[index][4] + " website is blank.")
+        print("\t" + ingr[index][3] + " website is blank.")
         print("\t -Length: " + len(str(soup)))
         return None
     matches = soup.find_all(ingr[index][1], class_=ingr[index][2])
