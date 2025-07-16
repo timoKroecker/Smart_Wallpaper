51966819e09c7d7f1dc3bf8ed6d6a1a7a6765c1a
timoKroecker <t.kroecker@web.de>
2025-07-16 17:53:52 +0200

    Format fix

--- finance_scraper.py
+++ finance_scraper.py
@@ -16,20 +16,25 @@ def get_months_finances(added_days):
     year_str = str(get_localtime(added_days).tm_year)
     income = dbi.select_monthly_total_income(month_str, year_str)
     expences = dbi.select_monthly_total_expenditure(month_str, year_str)
-    return [[income, expences, str(round(float(income) - float(expences), 2))],
-            mnths[month_int - 1][1]]
+    saldo = format_amount(str(round(float(income) - float(expences), 2)))
+    return [[income, expences, saldo], mnths[month_int - 1][1]]
 
 def get_years_finances(added_days):
     year_str = str(get_localtime(added_days).tm_year)
     income = dbi.select_yearly_total_income(year_str)
     expences = dbi.select_yearly_total_expenditure(year_str)
-    return [[income, expences, str(round(float(income) - float(expences), 2))],
-            year_str]
+    saldo = format_amount(str(round(float(income) - float(expences), 2)))
+    return [[income, expences, saldo], year_str]
 
 def get_percentage(category_amount, total_amount):
     if(total_amount == 0):
         return "0"
     return str(int(round(100 * float(category_amount) / float(total_amount), 0)))
+
+def format_amount(string):
+    if string[-2] == ".":
+        return string + "0"
+    return string
 
 def get_localtime(added_days):
     return time.localtime(time.time() + added_days * 86400)
